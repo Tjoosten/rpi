83db338e05b43bff7609edb8631c9a1dc1fec192
Tjoosten <Tjoosten3@gmail.com>
2015-06-10 09:50:43 +0200

    primitive email template for registration

--- laravel/resources/views/emails/register.blade.php
+++ laravel/resources/views/emails/register.blade.php
@@ -1,1 +1,12 @@
-Meh
+<p> Geachte,</p>
+
+<p>
+    Bedankt voor de intresse in ons en de registratie: <br />
+    Hier ondervind u nog is alle inlog gegevens.
+<p>
+
+<p>
+    <strong>Voornaam:</strong> {!! $firstname !!} <br />
+    <strong>Achternaam:</strong> {!! $lastname !!} <br />
+    <strong>Email:</strong> {!! $email !!}
+</p>
