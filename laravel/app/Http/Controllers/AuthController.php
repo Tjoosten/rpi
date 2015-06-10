84ad867069dd791ee157f5124a78ac41946de53f
Tjoosten <Tjoosten3@gmail.com>
2015-06-10 02:41:31 +0200

    start middleware implementation

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller {
      */
     public function __construct()
     {
-       // $this->middleware('admin');
+       $this->middleware('admin', ['only' => ['deleteUser']]);
        // $this->middleware('csrf');
     }
 
