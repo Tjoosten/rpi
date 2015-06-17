a49d9e1b7a6dfc771f2ef4cbe87e2438970de51b
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-06-17 20:43:21 +0200

    Update gulpfile.js

--- laravel/gulpfile.js
+++ laravel/gulpfile.js
@@ -1,4 +1,6 @@
-var elixir = require('laravel-elixir');
+var gulp   = require('gulp');
+var apidoc = require('gulp-apidoc');
+var elixer = require('laravel-elixer');
 
 /*
  |--------------------------------------------------------------------------
@@ -9,8 +11,34 @@ var elixir = require('laravel-elixir');
  | for your Laravel application. By default, we are compiling the Less
  | file for our application, as well as publishing vendor resources.
  |
+ | Commands: 
+ | =================
+ | Apidoc       = Generate API documentation
+ | Gulp         = Run all the tasks.
+ | Gulp styles  = Compile all the styling. 
+ | Gulp scripts = Compile all the scripts
+ | Gulp tdd     = Watch Tests And PHP Classes for Changes
  */
-
-elixir(function(mix) {
-    mix.less('app.less');
+ 
+ // Copy files to a new directory.
+ elixir(function(mix) {
+    mix.copy('vendor/package/views', 'resources/views');
+});
+ 
+ // Running phpunit.
+ elixir(function(mix) {
+    mix.phpUnit();
+});
+ 
+ // Compile less to css.
+ elixir(function(mix) {
+    mix.less('bootstrap.less');
+});
+ 
+ // Generate API documentation.
+ gulp.task('apidoc',function(){
+	apidoc.exec({
+	src: "App/Http/Controllers",
+	dest: "Resources/API-documentation",
+    debug: true,
 });
