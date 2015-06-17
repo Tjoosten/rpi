0563e3cbf7fbea3b56e5a2cd1c0ef241f607034d
Tjoosten <Tjoosten3@gmail.com>
2015-06-18 00:19:36 +0200

    add finel commit in branch

--- laravel/gulpfile.js
+++ laravel/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp   = require('gulp');
 var apidoc = require('gulp-apidoc');
-var elixer = require('laravel-elixer');
+var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
@@ -19,26 +19,13 @@ var elixer = require('laravel-elixer');
  | Gulp scripts = Compile all the scripts
  | Gulp tdd     = Watch Tests And PHP Classes for Changes
  */
- 
- // Copy files to a new directory.
- elixir(function(mix) {
+
+// Copy files to a new directory.
+elixir(function(mix) {
     mix.copy('vendor/package/views', 'resources/views');
 });
  
- // Running phpunit.
- elixir(function(mix) {
-    mix.phpUnit();
-});
- 
- // Compile less to css.
- elixir(function(mix) {
+// Compile less to css.
+elixir(function(mix) {
     mix.less('bootstrap.less');
-});
- 
- // Generate API documentation.
- gulp.task('apidoc',function(){
-	apidoc.exec({
-	src: "App/Http/Controllers",
-	dest: "Resources/API-documentation",
-    debug: true,
 });
